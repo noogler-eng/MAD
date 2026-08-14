03f6521870715317a28ac7004819e72aaf536182
noogler-eng <sharadpoddar10102003@gmail.com>
2026-08-15 00:25:01 +0530

    flutter basic

---
+++ flutter/first_app/lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+// import for debugging purposes
+import 'package:flutter/foundation.dart';
+
+// Entry point of the application
+// MyApp - The root widget of the application
+void main() {
+  runApp(const MyApp());
+}
+
+// Root widget of the application
+// Widget - stateless or stateful
+// always need to override the build method to describe how to display the widget in terms of other, lower-level widgets.
+
+// stateless widget - a widget with no internal mutable data. Given the same inputs, it always produces the same UI.
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+// stateful widget
+// combination of mutable and immutable widgets
+// MyHomePage + _MyHomePageState
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // widget state
+  int _counter = 0;
+
+  // normal function to increment the counter
+  void _incrementCounter() {
+    print('Incrementing counter from $_counter to ${_counter + 1}');
+    setState(() {
+      debugPrint('inside setState - Counter incremented to ${_counter + 1}');
+      _counter++;
+    });
+    debugPrint('Counter incremented to $_counter');
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      debugPrint('inside setState - Counter decremented to ${_counter - 1}');
+      _counter--;
+    });
+    debugPrint('Counter decremented to $_counter');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      // floating action button - a circular button that floats above the content of the screen
+      // both button push to the end of the screen
+      // every widgets can have children, but not all 
+      // widgets can have multiple children. For example, 
+      // a Row widget can have multiple children, but a Text 
+      // widget can only have one child.
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          )
+        ]
+      )
+    );
+  }
+}
