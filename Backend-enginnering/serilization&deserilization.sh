7ad30d48b52884d5615d6710c2995aa4150f0d76
noogler-eng <sharadpoddar10102003@gmail.com>
2026-08-26 08:16:03 +0530

    json serlization & deserlization

---
+++ Backend-enginnering/serilization&deserilization.sh
@@ -0,0 +1,21 @@
+# most popular serialization formats are JSON, XML, and YAML. 
+# Each format has its own advantages and disadvantages, and 
+# the choice of which to use depends on the specific requirements of the application.
+
+# types of serialization formats
+# Text-based formats: JSON, XML, YAML
+# Binary formats: Protocol Buffers, Avro, MessagePack
+
+# JAVASCRIPT OBJECT NOTATION (JSON)
+{
+    "name": "John Doe",
+    "age": 30,
+    "email": "john.doe@example.com",
+    "address": {
+        "street": "123 Main St",
+        "city": "Anytown",
+        "state": "CA",
+        "zip": 12345
+    }
+}
+
